c9384b42621ac5d3955d7977ee2ca7381d7e5d71
Belencitah <belen.poquioma@gmail.com>
2025-09-01 15:06:54 -0500

    xdfb

---
+++ ms-registry-server/src/main/java/com/example/msregistryserver/Algoritmos.java
@@ -0,0 +1,10 @@
+package com.example.msregistryserver;
+
+public class Algoritmos {
+
+    public int[] sumaDosNumeros(int[]m, int o){
+        int[] p=new int[2];
+        for (int i=0; i<=o;i++)
+        {}
+    }
+}
