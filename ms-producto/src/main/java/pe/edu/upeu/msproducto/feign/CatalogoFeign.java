b4bf1053b657b314db573234386faeb76d3a3de9
Belencitah <belen.poquioma@gmail.com>
2025-09-30 11:31:24 -0500

    feat(producto): update config y feign Catalogo

--- ms-producto/src/main/java/pe/edu/upeu/msproducto/feign/CatalogoFeign.java
+++ ms-producto/src/main/java/pe/edu/upeu/msproducto/feign/CatalogoFeign.java
@@ -1,5 +1,6 @@
 package pe.edu.upeu.msproducto.feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,5 +11,12 @@ import java.util.Optional;
 @FeignClient(name = "ms-catalogo", path = "/categoria")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "categoriaListarPorIdCB", fallbackMethod = "fallbackCategoria")
     public CatagoriaDto buscarPorId(@PathVariable Integer id);
+    default CatagoriaDto fallbackCategoria(Integer id, Exception e) {
+        CatagoriaDto catagoriaDto = new CatagoriaDto();
+        catagoriaDto.setId(9000000);
+        catagoriaDto.setNombre("Servicio Categoria no disponible");
+        return catagoriaDto;
+    }
 }
